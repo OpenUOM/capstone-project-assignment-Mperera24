2d6d32e4d3d3bb627bdbc856e149e136423f2e97
Mperera24 <k.malisha240406@gmail.com>
2022-10-20 07:12:50 +0530

    database function changes

--- backend/database.js
+++ backend/database.js
@@ -89,10 +89,88 @@ const deleteTeacher = async (id) => {
     });
 }
 
+
+const readStudents = async () => {
+    const sql = `SELECT * FROM dummyData`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+const readStudentInfo = async (id) => {
+    const sql = `SELECT * FROM dummyData`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+const addStudent = async (id, name, age, religion) => {
+    const sql = `SELECT * FROM dummyData`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+const updateStudent = async (name, age, religion, id) => {
+    const sql = `SELECT * FROM dummyData`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+} 
+
+const deleteStudent = async (id) => {
+    const sql = `SELECT * FROM dummyData`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+
 module.exports = {
     addTeacher,
     readTeachers,
     readTeacherInfo,
     updateTeacher,
-    deleteTeacher
+    deleteTeacher,
+
+    addStudent,
+    readStudents,
+    readStudentInfo,
+    updateStudent,
+    deleteStudent,
 }
